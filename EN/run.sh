5da8d5da138b772e21e99520da0bb27bfb873f1a
dophist <jerry.ejwt@qq.com>
2019-10-11 09:43:14 +0800

    minor renaming, correcting github language detection

--- EN/run.sh
+++ EN/run.sh
@@ -1,4 +1,4 @@
-iconv -f ISO_8859-10 -t utf8 cmudict-0.7b > tmp # convert raw format from IOS-8859 to UTF8
+iconv -f ISO_8859-10 -t utf8 cmudict-0.7b.txt > tmp # convert raw format from IOS-8859 to UTF8
 #python ../utils/dict_to_phoneset.py tmp ARPA.list
 python ../utils/map_arpa_to_ipa.py ARPA2IPA.map tmp EN.txt
 rm tmp
